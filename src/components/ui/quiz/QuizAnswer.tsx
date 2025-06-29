7280d42243b4971184ef9b1376168d7fcc9f347b
kavinsk23 <23.kavinsk@gmail.com>
2025-06-29 22:20:00 +0530

    quiz 2 completed

---
+++ src/components/ui/quiz/QuizAnswer.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import correct from "../../../assets/icons/correct.svg";
+import wrong from "../../../assets/icons/wrong.svg";
+
+interface OptionProps {
+  title: string;
+  image: string;
+}
+
+const QuizAnswer: React.FC<OptionProps> = ({ title, image }) => {
+  return (
+    <div className="relative p-6 rounded-xl cursor-pointer transition-all duration-200 bg-renault-bg-section h-52 w-52 flex items-center justify-center">
+      <div className="text-center flex flex-col items-center">
+        <h3 className="text-base font-dm-sans text-renault-text-primary leading-snug">
+          {title}
+        </h3>
+        <img
+          src={image}
+          alt=""
+          className="w-10 h-10 mb-4 object-contain absolute bottom-0 right-2"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default QuizAnswer;
